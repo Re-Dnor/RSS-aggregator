b44a68502e3c77ba1609ed991eccad04493be924
Re-Dnor <MokshinRE@yandex.ru>
2022-06-01 17:17:24 +0500

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export default () => ({
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(path.resolve(), "./dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+  ]
+});
