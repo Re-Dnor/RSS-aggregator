33131e52abf751cc0307464766830e2b06056dd4
Re-Dnor <MokshinRE@yandex.ru>
2022-06-07 15:29:49 +0500

    refactor: change structure of the project

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,14 +1,13 @@
 import * as yup from 'yup';
 
 export default (urlValidated, list) => {
-
   const schema = yup
     .string()
-    .required()
-    .url('Ссылка должна быть валидным URL')
+    .required('empty')
+    .url('invalidURL')
     .notOneOf(
       list.map((link) => link),
-      'RSS уже существует',
+      'duplicate',
     );
   return schema.validate(urlValidated.trim());
 };
