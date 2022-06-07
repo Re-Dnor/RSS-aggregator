ece422f104f9d2db568228fb98d30172d00fc276
Re-Dnor <MokshinRE@yandex.ru>
2022-06-07 15:30:28 +0500

    feat: add languages (ru, en)

---
+++ src/locales/en.js
@@ -0,0 +1,31 @@
+export default {
+  translation: {
+    header: {
+      title: 'RSS aggregator',
+      subtitle: 'Start reading RSS today!',
+    },
+    form: {
+      button: 'Add',
+      label: 'RSS link',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      successMessage: 'RSS has been successfully loaded',
+      errors: {
+        invalidURL: 'Link must be a valid URL',
+        duplicate: 'RSS already exists',
+        main: 'Something went wrong',
+        empty: 'Must not be empty',
+      },
+      posts: {
+        title: 'Posts',
+        postLink: 'View',
+      },
+      feeds: {
+        title: 'Feeds',
+      },
+      modal: {
+        read: 'Read more',
+        close: 'Close',
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,31 @@
+export default {
+  translation: {
+    header: {
+      title: 'RSS агрегатор',
+      subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    },
+    form: {
+      button: 'Добавить',
+      label: 'RSS Ссылка',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+      successMessage: 'RSS успешно загружен',
+      errors: {
+        invalidURL: 'Ссылка должна быть валидным URL',
+        duplicate: 'RSS уже существует',
+        main: 'Что-то пошло не так',
+        empty: 'Не должно быть пустым',
+      },
+      posts: {
+        title: 'Посты',
+        postLink: 'Просмотр',
+      },
+      feeds: {
+        title: 'Фиды',
+      },
+      modal: {
+        read: 'Читать полностью',
+        close: 'Закрыть',
+      },
+    },
+  },
+};
