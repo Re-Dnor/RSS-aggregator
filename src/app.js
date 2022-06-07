c054c50658dc83ba03f56d5d2ea6964de940b801
Re-Dnor <MokshinRE@yandex.ru>
2022-06-07 15:30:53 +0500

    feat: used i18next

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
-import onChange from "on-change";
-import validate from "./validate.js";
-import render from './render.js'
+import i18next from 'i18next';
+import onChange from 'on-change';
+import validate from './utils/validate.js';
+import render from './render.js';
+import resources from './locales/languages.js';
 
-export default () => {
+export default async () => {
+  const defaultLanguages = 'ru';
+  const i18nextInstance = i18next.createInstance();
+  await i18nextInstance.init({
+    lng: defaultLanguages,
+    debug: false,
+    resources,
+  });
+
   const elements = {
     form: document.getElementById('rss-form'),
     input: document.getElementById('rss-input'),
-    feedback: document.querySelector('.feedback')
-  }
+    feedback: document.querySelector('.feedback'),
+    title: document.querySelector('h1'),
+    subtitle: document.querySelector('.lead'),
+    button: document.querySelector('.col-auto'),
+    example: document.querySelector('.text-muted'),
+    label: document.querySelector('.text-dark'),
+  };
 
   const state = {
     form: {
@@ -24,7 +39,7 @@ export default () => {
     language: null,
   };
 
-  const watchedState = onChange(state, render(elements))
+  const watchedState = onChange(state, render(elements, state, i18nextInstance));
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -32,16 +47,34 @@ export default () => {
     const formData = new FormData(e.target);
     const url = formData.get('rss-input');
 
-    validate(url, watchedState.data.feeds)
-      .then((url) => {
+    validate(url, watchedState.data.feeds, i18nextInstance)
+      .then((link) => {
         watchedState.form.feedback.error = null;
         watchedState.form.processState = 'success';
-        watchedState.data.feeds.push(url)
+        watchedState.form.feedback.success = true;
+        watchedState.data.feeds.push(link);
+
+        elements.form.reset();
       })
       .catch((error) => {
+        watchedState.form.feedback.success = false;
         watchedState.form.feedback.error = error;
         watchedState.form.processState = 'fail';
-        console.log(error.message)
-      })
-  })
-}
+        // console.log(error)
+
+        elements.input.focus();
+      });
+  });
+
+  const btnLanguage = document.querySelector('.language-change');
+
+  btnLanguage.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentLanguage = i18nextInstance.language;
+    const newLanguage = currentLanguage === 'ru' ? 'en' : 'ru';
+
+    i18nextInstance.changeLanguage(newLanguage);
+    watchedState.language = newLanguage;
+    btnLanguage.textContent = currentLanguage;
+  });
+};
--- src/render.js
+++ src/render.js
@@ -1,38 +1,67 @@
-const renderError = (elements, value) => {
-  const feedback = elements.feedback;
-  const input = elements.input
-
+// RENDER ERROR____________________________
+const renderError = (elements, value, i18nextInstance) => {
+  const { feedback, input } = elements;
   if (value instanceof Error) {
-    feedback.textContent = value.message;
-    feedback.classList.remove('text-success')
+    feedback.textContent = i18nextInstance.t(`form.errors.${value.message}`);
+    feedback.classList.remove('text-success');
     feedback.classList.add('text-danger');
-    input.classList.remove('text-success')
-    input.classList.add('is-invalid')
+    input.classList.remove('text-success');
+    input.classList.add('is-invalid');
+  }
+};
+// RENDER SUCCESS_________________________________________
+const renderSuccess = (elements, value, i18nextInstance) => {
+  const { feedback, input } = elements;
+  if (value) {
+    feedback.textContent = i18nextInstance.t('form.successMessage');
+    feedback.classList.add('text-success');
+    feedback.classList.remove('text-danger');
+    input.classList.remove('is-invalid');
+    input.classList.add('text-success');
+    setTimeout(() => input.classList.remove('text-success'), 2000);
   } else {
     feedback.textContent = '';
-    feedback.classList.add('text-success')
-    feedback.classList.remove('text-danger');
-    input.classList.remove('is-invalid')
-    input.classList.add('text-success')
+    input.classList.remove('text-success');
   }
-}
+};
+// RENDER FORM_____________________________
+const renderForm = (elements, value, i18nextInstance) => {
+  const {
+    label, button, title, subtitle, example,
+  } = elements;
 
-const handleProcessState = (elements, value) => {
-  const { form, input, feedback } = elements
-  switch (value) {
-    case 'sending':
-      feedback.textContent = '';
+  label.textContent = i18nextInstance.t('form.label');
+  button.textContent = i18nextInstance.t('form.button');
+  title.textContent = i18nextInstance.t('header.title');
+  subtitle.textContent = i18nextInstance.t('header.subtitle');
+  example.textContent = i18nextInstance.t('form.example');
+};
+
+// MAIN RENDER________________________________
+const render = (elements, state, i18nextInstance) => (path, value) => {
+  // console.log(path)
+  switch (path) {
+    case 'form.processState':
+      renderForm(elements, value, i18nextInstance);
+      break;
+    case 'form.feedback.error':
+      renderError(elements, value, i18nextInstance);
+      break;
+    case 'form.feedback.success':
+      renderSuccess(elements, value, i18nextInstance);
+      break;
+    case 'language':
+      renderForm(elements, value, i18nextInstance);
+      if (state.form.feedback.error) {
+        renderError(elements, state.form.feedback.error, i18nextInstance);
+      }
+      if (state.form.feedback.success) {
+        renderSuccess(elements, state.form.feedback.success, i18nextInstance);
+      }
+      break;
+    default:
       break;
   }
-}
-
-const render = (elements) => (path, value) => {
-
-  switch (path) {
-    case 'form.feedback.error':
-      renderError(elements, value)
-  }
-
-}
+};
 
 export default render;
