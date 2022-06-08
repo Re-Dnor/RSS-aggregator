90d8fef9cfa36ca66ac8c3504f8736c4f6f9ff35
Re-Dnor <MokshinRE@yandex.ru>
2022-06-08 17:51:23 +0500

    feat: create posts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import i18next from 'i18next';
 import onChange from 'on-change';
+import _ from 'lodash';
 import validate from './utils/validate.js';
 import render from './render.js';
 import resources from './locales/languages.js';
+import getData from './utils/getData.js';
+import parser from './utils/parser.js';
 
 export default async () => {
   const defaultLanguages = 'ru';
@@ -36,23 +39,36 @@ export default async () => {
       feeds: [],
       posts: [],
     },
-    language: null,
+    language: defaultLanguages,
   };
 
   const watchedState = onChange(state, render(elements, state, i18nextInstance));
-
+  // ADD EVENT LISTENER FORM________________________
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.target);
     const url = formData.get('rss-input');
 
     validate(url, watchedState.data.feeds, i18nextInstance)
-      .then((link) => {
+      .then((link) => getData(link))
+      .then((response) => {
+        const { contents } = response.data;
+        const { title, description, posts } = parser(contents);
+        const id = _.uniqueId();
+
+        watchedState.data.feeds.unshift({
+          title,
+          description,
+          url,
+          id,
+        });
+        const getIdPosts = [...posts.map((post) => ({ ...post, feedID: id }))];
+        const allPosts = [...watchedState.data.posts, ...getIdPosts];
+        watchedState.data.posts = allPosts;
         watchedState.form.feedback.error = null;
         watchedState.form.processState = 'success';
         watchedState.form.feedback.success = true;
-        watchedState.data.feeds.push(link);
+        console.log(state);
 
         elements.form.reset();
       })
@@ -60,12 +76,12 @@ export default async () => {
         watchedState.form.feedback.success = false;
         watchedState.form.feedback.error = error;
         watchedState.form.processState = 'fail';
-        // console.log(error)
 
         elements.input.focus();
       });
   });
 
+  // CHANGE LANG ADD E LISTENER _________________________________
   const btnLanguage = document.querySelector('.language-change');
 
   btnLanguage.addEventListener('click', (e) => {
