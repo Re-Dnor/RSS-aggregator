82e4b5a64b29d6f2430ffd0dac0c3ee157dbff0a
Re-Dnor <MokshinRE@yandex.ru>
2022-06-09 09:43:22 +0500

    feat: render modal

--- src/utils/renderModal.js
+++ src/utils/renderModal.js
@@ -1,3 +1,15 @@
-// export default (title, description, link, i18nextInstance) => {
-//   const modal = document.querySelector('.modal');
-// };
+export default (title, description, link, i18nextInstance) => {
+  const modal = document.querySelector('.modal-content');
+  const modalTitle = modal.querySelector('.modal-title');
+  const modalDescription = modal.querySelector('.modal-body');
+  const modalBtnLink = modal.querySelector('#btn-link');
+  const modalWrapperLink = document.getElementById('link');
+  const modalBtnClose = modal.querySelector('.btn-secondary');
+
+  modalTitle.textContent = title;
+  modalDescription.textContent = description;
+  modalBtnLink.textContent = i18nextInstance.t('form.modal.read');
+  modalWrapperLink.href = link;
+  modalWrapperLink.setAttribute('target', '_blank');
+  modalBtnClose.textContent = i18nextInstance.t('form.modal.close');
+};
