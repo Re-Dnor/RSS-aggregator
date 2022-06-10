b93bd5a0a8269fdecd449bee6042f0bfbfc2d6fd
Re-Dnor <MokshinRE@yandex.ru>
2022-06-10 23:57:39 +0500

    feat: correct app.js

--- src/app.js
+++ src/app.js
@@ -85,7 +85,7 @@ export default async () => {
         watchedState.form.processState = 'success';
         watchedState.form.feedback.success = true;
 
-        elements.form.reset();
+        elements.form.value = '';
 
         elements.button.disabled = false;
 
