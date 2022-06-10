9c70f88341cc52bfbead5ff2445d21cc2e4d7478
Re-Dnor <MokshinRE@yandex.ru>
2022-06-10 21:50:11 +0500

    refactor: switch placement

--- src/render/renderSuccess.js
+++ src/render/renderSuccess.js
@@ -1,12 +1,13 @@
 export default (elements, value, i18nextInstance) => {
   const { feedback, input } = elements;
   if (value) {
-    feedback.textContent = i18nextInstance.t('form.successMessage');
     feedback.classList.add('text-success');
     feedback.classList.remove('text-danger');
     input.classList.remove('is-invalid');
+    feedback.textContent = i18nextInstance.t('form.successMessage');
   } else {
-    feedback.textContent = '';
     input.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.textContent = '';
   }
 };
